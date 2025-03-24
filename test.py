32c8ab3e5444ed934ae385511fb1b32ccf398ca0
Aneesh-G7 <gsaneesh65@gmail.com>
2025-03-24 11:36:22 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,5 @@
+from image_effects import apply_blur, apply_grayscale
+
+# Example test:
+apply_blur("input/test.jpg", "output/test_blur.png")
+apply_grayscale("input/test.jpg", "output/test_gray.png")
